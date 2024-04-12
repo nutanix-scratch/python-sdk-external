afcf39a03bf8ae97460f51ef47b6265bd5386cb9
API-Infra-Build-User <svc.canGit@nutanix.com>
2024-04-12 08:32:34 +0000

    dataprotection 4.0.1b1 [2024-04-12 08:32:34.354157]

---
+++ dataprotection/ntnx_dataprotection_py_client/api/__init__.py
@@ -0,0 +1,9 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_dataprotection_py_client.api.consistency_groups_api import ConsistencyGroupsApi
+from ntnx_dataprotection_py_client.api.protected_resources_api import ProtectedResourcesApi
+from ntnx_dataprotection_py_client.api.recovery_points_api import RecoveryPointsApi
